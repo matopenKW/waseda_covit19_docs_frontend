28c63d44f8655f42645616502d17076fe6f54408
小野和論 <matopenkw@gmail.com>
2020-12-08 12:17:13 +0900

    create new project.

---
+++ src/pages/testpage/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="container">
+      {{ message }}
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      message: 'hello',
+    }
+  },
+
+}
+</script>
