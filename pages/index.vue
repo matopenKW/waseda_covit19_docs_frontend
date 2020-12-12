3a756901f88426000121ba8fc87467986dcd81b0
matopenKW <40521317+matopenKW@users.noreply.github.com>
2020-12-12 15:10:00 +0900

    create history page (#6)

--- pages/index.vue
+++ pages/index.vue
@@ -1,119 +1,69 @@
 <template>
-    <div id="login-form" class="login">
-        <h2 class="login-header">ログイン</h2>
-        <form class="login-container">
-            <p><input type="email" placeholder="Email"></p>
-            <p><input type="password" placeholder="Password"></p>
-            <p><input type="button" value="Log in" @click="submitLogin"></p>
-            <p><input type="button" value="Gest User" @click="submitGetUser"></p>
-        </form>
-    </div>
-</template>
-<script>
+  <div class="container">
 
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                  <form class="user">
+                    <div class="form-group">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <a href="/mock2" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </a>
+                    <hr>
+                    <a href="/mock2" class="btn btn-google btn-user btn-block">
+                      <i class="fab fa-google fa-fw"></i> Login with Google
+                    </a>
+                    <a href="/mock2" class="btn btn-facebook btn-user btn-block">
+                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                    </a>
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Create an Account!</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
 export default {
     layout ({ store }) {
         return 'login'
     },
-    data: function() {
-        return {
-            files: []
-        }
-    },
-    mounted: function () {
-        this.$nextTick(function () {
-            console.log('load')
-        })
-    },
-    methods: {
-        async submitLogin(){
-            alert('click Login')
-        },
-        async submitGetUser(){
-            window.location.href = '/campTool/top'
-        }
-    }
 }
-
 </script>
-
-<style>
-
-#login-form {
-    margin-top: 7vh;
-}
-
-.login {
-  width: 400px;
-  margin: 16px auto;
-  font-size: 16px;
-}
-
-/* Reset top and bottom margins from certain elements */
-.login-header,
-.login p {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-
-.login-header {
-  background: #28d;
-  padding: 20px;
-  font-size: 1.4em;
-  font-weight: normal;
-  text-align: center;
-  text-transform: uppercase;
-  color: #fff;
-}
-
-.login-container {
-  background: #ebebeb;
-  padding: 12px;
-}
-
-/* Every row inside .login-container is defined with p tags */
-.login p {
-  padding: 12px;
-}
-
-.login input {
-  box-sizing: border-box;
-  display: block;
-  width: 100%;
-  border-width: 1px;
-  border-style: solid;
-  padding: 16px;
-  outline: 0;
-  font-family: inherit;
-  font-size: 0.95em;
-}
-
-.login input[type="email"],
-.login input[type="password"] {
-  background: #fff;
-  border-color: #bbb;
-  color: #555;
-}
-
-/* Text fields' focus effect */
-.login input[type="email"]:focus,
-.login input[type="password"]:focus {
-  border-color: #888;
-}
-
-.login input[type="button"] {
-  background: #28d;
-  border-color: transparent;
-  color: #fff;
-  cursor: pointer;
-}
-
-.login input[type="button"]:hover {
-  background: #17c;
-}
-
-/* Buttons' focus effect */
-.login input[type="button"]:focus {
-  border-color: #05a;
-}
-</style>
--- pages/mock2/index.vue
+++ pages/mock2/index.vue
@@ -26,7 +26,6 @@
         </div>
       </div>
 
-      <!-- Earnings (Monthly) Card Example -->
       <div class="col-xl-3 col-md-6 mb-4" @click="route">
         <div class="card border-left-success shadow h-100 py-2">
           <div class="card-body">
@@ -42,8 +41,23 @@
         </div>
       </div>
 
+      <div class="col-xl-3 col-md-6 mb-4" @click="history">
+        <div class="card border-left-info shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="h5 mb-0 font-weight-bold text-gray-800">History</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-user fa-2x text-gray-300"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="col-xl-3 col-md-6 mb-4" @click="admin">
-        <div class="card border-left-success shadow h-100 py-2">
+        <div class="card border-left-danger shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
@@ -71,6 +85,9 @@ export default {
         },
         async route(){
             window.location.href = '/mock2/route'
+        },
+        async history(){
+            window.location.href = '/mock2/history'
         },
         async admin(){
             window.location.href = '/mock2/admin'
