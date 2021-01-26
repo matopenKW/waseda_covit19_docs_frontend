c40b22beb337de2a011521217e7ec81034481006
shotaro <sn1214034@gmail.com>
2021-01-26 22:53:36 +0900

    no message

--- pages/mock2/contact.vue
+++ pages/mock2/contact.vue
@@ -113,3 +113,72 @@
     <!-- /.p-contact__form -->
     </div>
 </template>
+
+<!-- 以下コピペ　全く理解してません -->
+<script>
+  export default {
+    data() {
+      return {
+        username        : '',
+        katakana        : '',
+        useremail       : '',
+        message         : '',
+        botField        : '',
+        isSubmit        : false,
+        isSending       : false,
+        isError         : false,
+        completeMessage : '',
+      }
+    },
+    computed: {
+      sendingClass(){
+        return {
+          'is-sending'  : this.isSending,
+          'is-error'    : this.isError,
+          'is-complete' : this.isSubmit
+        };
+      }
+    },
+    methods: {
+      onSubmit() {
+        if(this.isSending){
+          return;
+        }
+        this.isSending = true;
+        this.completeMessage = '送信処理中…';
+        const params = new URLSearchParams();
+        params.append('form-name', 'contact');
+        params.append('username', this.username);
+        params.append('katakana', this.katakana);
+        params.append('useremail', this.useremail);
+        params.append('message', this.message);
+        if(this.botField){
+          params.append('bot-field', this.botField);
+        }
+        this.$axios
+        .$post('/', params)
+        .then(() => {
+          this.completeMessage = 'お問い合わせを送信しました！';
+          this.resetForm();
+          this.isSubmit  = true;
+        })
+        .catch(err => {
+          this.completeMessage = 'お問い合わせの送信が失敗しました';
+          this.isError   = true;
+        })
+        .finally(() => {
+          this.isSending = false;
+        });
+      },
+
+      resetForm() {
+        this.username        = '';
+        this.katakana        = '';
+        this.useremail       = '';
+        this.message         = '';
+        this.isError         = false;
+        this.$refs.observer.reset();
+      }
+    }
+  }
+</script>
