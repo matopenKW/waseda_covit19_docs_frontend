262e303f497cbddfaec85df13bb4cb3409ef7afd
小野和論 <matopenkw@gmail.com>
2021-03-13 19:15:02 +0900

    fix urls

--- plugins/urls.js
+++ plugins/urls.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
 Vue.prototype.$urls = {
-    api: 'https://waseda-covite19-docs.herokuapp.com',
+    api: 'https://waseda-covite19-docs.herokuapp.com/api/v1',
     firebase: 'https://waseda-covit19-docs-firebase.herokuapp.com',
 }
